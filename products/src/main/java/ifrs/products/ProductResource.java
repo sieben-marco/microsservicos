fd8f2140a8f12add1b103b7f4ea81409282b3be4
sieben-marco <sociais.marco@gmail.com>
2025-11-19 12:14:19 -0300

    feat: adicionar ao resource de produtos metodos HTTP GET para listar os produtos e obter um produto especifico

--- products/src/main/java/ifrs/products/ProductResource.java
+++ products/src/main/java/ifrs/products/ProductResource.java
@@ -1,5 +1,10 @@
 package ifrs.products;
 
+import java.util.List;
+
+import org.jboss.resteasy.reactive.RestResponse;
+
+import ifrs.products.models.Product;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,5 +13,20 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/products")
 public class ProductResource {
 
-    
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public RestResponse<List<Product>> list() {
+    return RestResponse.ok(Product.listAll());
+  }
+
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public RestResponse<Product> get(Long id) {
+    Product entity = Product.findById(id);
+    if (entity == null) {
+      return RestResponse.notFound();
+    }
+    return RestResponse.ok(entity);
+  }
 }
