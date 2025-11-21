a30fe408d1b9218785779f0806b52dfea7368c77
sieben-marco <sociais.marco@gmail.com>
2025-11-21 12:11:27 -0300

    refactor: alterar retornos para Jakarta Response

--- products/src/main/java/ifrs/products/ProductResource.java
+++ products/src/main/java/ifrs/products/ProductResource.java
@@ -1,32 +1,36 @@
 package ifrs.products;
 
-import java.util.List;
-
-import org.jboss.resteasy.reactive.RestResponse;
-
 import ifrs.products.models.Product;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
 
 @Path("/products")
 public class ProductResource {
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public RestResponse<List<Product>> list() {
-    return RestResponse.ok(Product.listAll());
+  public Response list() {
+    return Response.ok(Product.listAll())
+        .build();
   }
 
   @GET
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public RestResponse<Product> get(Long id) {
+  public Response get(Long id) {
     Product entity = Product.findById(id);
     if (entity == null) {
-      return RestResponse.notFound();
+      return Response.status(Status.NOT_FOUND)
+          .build();
     }
-    return RestResponse.ok(entity);
+    return Response.ok(entity)
+        .build();
   }
+
+  
 }
