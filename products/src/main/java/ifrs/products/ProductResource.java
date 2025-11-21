387568d311ab0291c36dfcd071245b5e3f59c910
sieben-marco <sociais.marco@gmail.com>
2025-11-21 12:14:30 -0300

    feat: adicionar metodos http faltantes para concluir CRUD

--- products/src/main/java/ifrs/products/ProductResource.java
+++ products/src/main/java/ifrs/products/ProductResource.java
@@ -1,13 +1,22 @@
 package ifrs.products;
 
 import ifrs.products.models.Product;
+
+import java.net.URI;
+import java.util.Map;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
-
+import jakarta.ws.rs.core.UriBuilder;
 
 @Path("/products")
 public class ProductResource {
@@ -32,5 +41,54 @@ public class ProductResource {
         .build();
   }
 
-  
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @Transactional
+  public Response create(Product newProduct) {
+    if (newProduct.id != null) {
+      final int UNPROCESSABLE_ENTITY = 422;
+      return Response.status(UNPROCESSABLE_ENTITY)
+          .entity(Map.of("message", "O ID deve ser nulo ao criar um novo produto."))
+          .build();
+    }
+    Product.persist(newProduct);
+    URI uri = UriBuilder.fromPath("/products/{id}")
+        .build(newProduct.id);
+    return Response.created(uri)
+        .entity(newProduct)
+        .build();
+  }
+
+  @PUT
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @Transactional
+  public Response update(Long id, Product updatedProduct) {
+    Product entity = Product.findById(id);
+    if (entity == null) {
+      return Response.status(Status.NOT_FOUND)
+          .build();
+    }
+    entity.name = updatedProduct.name;
+    entity.description = updatedProduct.description;
+    entity.price = updatedProduct.price;
+    return Response.ok(entity)
+        .build();
+  }
+
+  @DELETE
+  @Path("/{id}")
+  @Transactional
+  public Response delete(Long id) {
+    Product entity = Product.findById(id);
+    if (entity == null) {
+      return Response.status(Status.NOT_FOUND)
+          .build();
+    }
+    entity.delete();
+    return Response.noContent()
+        .build();
+  }
 }
