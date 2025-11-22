56ad9954c6fb5bedee74c09a70bb9b1451c5d814
sieben-marco <sociais.marco@gmail.com>
2025-11-22 13:13:24 -0300

    feat: impedir que o nome do produto seja nulo

--- products/src/main/java/ifrs/products/models/Product.java
+++ products/src/main/java/ifrs/products/models/Product.java
@@ -1,10 +1,13 @@
 package ifrs.products.models;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Product extends PanacheEntity {
+
+  @Column(nullable = false)
   public String name;
   public String description;
   public double price;
