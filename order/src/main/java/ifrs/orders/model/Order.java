f7a5979e702d42cacbc3c9fa90542d270cff85dd
sieben-marco <sociais.marco@gmail.com>
2025-11-23 00:48:09 -0300

    feat: criar entidade para os pedidos

---
+++ order/src/main/java/ifrs/orders/model/Order.java
@@ -0,0 +1,24 @@
+package ifrs.orders.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order extends PanacheEntity{
+  public String status; // ABERTO, PAGO
+  public Double total;
+
+  @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+  @JoinColumn(name = "orderId")
+  public List<Item> items = new ArrayList<>();
+}
